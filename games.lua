a060e853af11b4fcfd667d6be464f57bcbe0c993
Pyerkepping <vampyisreal@gmail.com>
2024-11-16 08:12:47 -0800

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,3 @@
+return {
+    { name = "Counter Blox", gameid = 301549746, link = "https://raw.githubusercontent.com/Pyerkepping/Holder/refs/heads/main/counterBloxSRC.lua?token=GHSAT0AAAAAAC2SCXVYXAQJDPRV4RLVDZYSZZYQ63A" }
+}
